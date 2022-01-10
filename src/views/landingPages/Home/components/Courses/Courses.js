baa85b497c257fb15f33d651e8b6c9538c5f3c58
Javier Rivas <javier.rivas@devo.com>
2022-01-10 19:56:36 +0100

    add courses diagram via divs and related positions

--- src/views/landingPages/Home/components/Courses/Courses.js
+++ src/views/landingPages/Home/components/Courses/Courses.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import { useTheme, alpha } from '@mui/material/styles';
-import { Box } from '@mui/system';
-import Container from 'common/Container';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import { Button } from '@mui/material';
-import { colors } from '@mui/material';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import CardMedia from '@mui/material/CardMedia';
-import Avatar from '@mui/material/Avatar';
+import React from "react";
+import { useTheme, alpha } from "@mui/material/styles";
+import { Box } from "@mui/system";
+import Container from "common/Container";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import { Button } from "@mui/material";
+import { colors } from "@mui/material";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import CardMedia from "@mui/material/CardMedia";
+import Avatar from "@mui/material/Avatar";
 
-import entrenamientoRelajacionCreativa from 'img/libros/relajacionCreativa.png';
-import sabiduriaDeLasEmociones from 'img/libros/sabiduriaDeLasEmociones.png';
-import creenciasDanianCreenciasSanas from 'img/libros/creenciasDanianCreenciasSanas.png';
-import miedoAlBienestar from 'img/libros/miedoAlBienestar.jpg';
-import laLlaveOlvidada from 'img/libros/laLlaveOlvidada.png';
+import entrenamientoRelajacionCreativa from "img/libros/relajacionCreativa.png";
+import sabiduriaDeLasEmociones from "img/libros/sabiduriaDeLasEmociones.png";
+import creenciasDanianCreenciasSanas from "img/libros/creenciasDanianCreenciasSanas.png";
+import miedoAlBienestar from "img/libros/miedoAlBienestar.jpg";
+import laLlaveOlvidada from "img/libros/laLlaveOlvidada.png";
 
-import MenuBookIcon from '@mui/icons-material/MenuBook';
-import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 
 const Courses = (props) => {
   const theme = useTheme();
@@ -35,9 +35,9 @@ const Courses = (props) => {
                 component={Typography}
                 fontWeight={700}
                 color={theme.palette.secondary.main}
-                fontStyle={'italic'}
-                variant={'h4'}
-                align={'center'}
+                fontStyle={"italic"}
+                variant={"h4"}
+                align={"center"}
                 gutterBottom
                 data-aos="fade-up"
               >
@@ -47,49 +47,54 @@ const Courses = (props) => {
             <Grid container spacing={2}>
               {[
                 {
-                  title: 'Entrenamiento en Relajación Creativa - Dr. Herrero Lozano, Eugenio.',
+                  title:
+                    "Entrenamiento en Relajación Creativa - Dr. Herrero Lozano, Eugenio.",
                   description: [
                     'Impartimos cursos diseñados por nosotros, dirigidos a personas interesadas en el desarrollo de la salud emocional. De ahí nuestra idea de "Enseñamos Salud',
-                    'Los cursos están organizados secuencialmente. El curso base sobre el que se apoya toda la estructura de nuestro trabajo es el Entrenamiento en Relajación Creativa.',
-                    'Éste es complementado con el curso de Entrenamiento en Habilidades Emocionales.',
-                    'A partir de este nivel se pueden realizar dos itinerarios:',
-                    'Uno dirigido al desarrollo de la Consciencia, profundizando en la relajación y la meditación. Son los cursos de Consciencia Creativa, Consciencia de la Consciencia y Consciencia de la Unidad.',
-                    'Por otro lado tenemos los cursos de Creencias y de Miedo al Bienestar, orientados al trabajo con las emociones y pensamientos que nos dañan.',
-                    'De algunos de estos cursos se realizan Reuniones de Seguimiento periódicas en las que se trabaja a un nivel más profundo las estrategias aprendidas en los diferentes cursos.'
+                    "Los cursos están organizados secuencialmente. El curso base sobre el que se apoya toda la estructura de nuestro trabajo es el Entrenamiento en Relajación Creativa.",
+                    "Éste es complementado con el curso de Entrenamiento en Habilidades Emocionales.",
+                    "A partir de este nivel se pueden realizar dos itinerarios:",
+                    "Uno dirigido al desarrollo de la Consciencia, profundizando en la relajación y la meditación. Son los cursos de Consciencia Creativa, Consciencia de la Consciencia y Consciencia de la Unidad.",
+                    "Por otro lado tenemos los cursos de Creencias y de Miedo al Bienestar, orientados al trabajo con las emociones y pensamientos que nos dañan.",
+                    "De algunos de estos cursos se realizan Reuniones de Seguimiento periódicas en las que se trabaja a un nivel más profundo las estrategias aprendidas en los diferentes cursos.",
                   ],
-                  date: '1987'
-                }
+                  date: "1987",
+                },
               ].map((item, i) => (
-                <Grid item xs={12} key={i} data-aos={'fade-up'}>
+                <Grid item xs={12} key={i} data-aos={"fade-up"}>
                   <Box
                     component={Card}
-                    display={'flex'}
-                    flexDirection={{ xs: 'column', md: 'row' }}
-                    variant={'outlined'}
+                    display={"flex"}
+                    flexDirection={{ xs: "column", md: "row" }}
+                    variant={"outlined"}
                     padding={2}
                     boxShadow={0}
-                    bgcolor={'transparent'}
+                    bgcolor={"transparent"}
                     border={0}
                     sx={{
-                      transition: 'all .2s ease-in-out',
-                      '&:hover': {
+                      transition: "all .2s ease-in-out",
+                      "&:hover": {
                         transform: `translateY(-${theme.spacing(1 / 2)})`,
                       },
                     }}
                   >
                     <Box
                       component={CardContent}
-                      bgcolor={'transparent'}
+                      bgcolor={"transparent"}
                       marginTop={-5}
                     >
-                      <Box component={Card} borderRadius={2} paddingLeft={5} boxShadow={'8px 8px 8px 0px rgb(140 152 164 / 25%)'}>
+                      <Box
+                        component={Card}
+                        borderRadius={2}
+                        paddingLeft={5}
+                        boxShadow={"8px 8px 8px 0px rgb(140 152 164 / 25%)"}
+                      >
                         <CardContent>
                           <Box
-                            display={'flex'}
-                            justifyContent={'space-between'}
-                            align={'center'}
-                          >
-                          </Box>
+                            display={"flex"}
+                            justifyContent={"space-between"}
+                            align={"center"}
+                          ></Box>
                           <Box marginTop={1}>
                             <Grid container spacing={1}>
                               {item.description?.map((lesson, j) => (
@@ -97,12 +102,12 @@ const Courses = (props) => {
                                   <Box
                                     component={ListItem}
                                     disableGutters
-                                    width={'auto'}
+                                    width={"auto"}
                                     padding={0}
                                   >
                                     <Box
                                       component={ListItemAvatar}
-                                      minWidth={'auto !important'}
+                                      minWidth={"auto !important"}
                                       marginRight={2}
                                     >
                                       <ArrowRightIcon color={"secondary"} />
@@ -121,11 +126,180 @@ const Courses = (props) => {
               ))}
             </Grid>
           </Box>
-
+          <Box
+            border="1px solid green"
+            width="100%"
+            display={"flex"}
+            justifyContent={"center"}
+            flexDirection="column"
+            alignContent="center"
+            alignItems="center"
+            height="400px"
+          >
+            <Box
+              display={"flex"}
+              fontWeight={700}
+              color={theme.palette.secondary.main}
+              fontStyle={"italic"}
+              variant={"h4"}
+              align={"center"}
+              gutterBottom
+              data-aos="fade-up"
+              border="1px solid red"
+              width="fit-content"
+              maxWidth="180px"
+            >
+              Entrenamiento en relajación creativa
+            </Box>
+            <Box
+              display={"flex"}
+              backgroundColor="blue"
+              width="10px"
+              height="40px"
+            ></Box>
+            <Box
+              display={"flex"}
+              fontWeight={700}
+              color={theme.palette.secondary.main}
+              fontStyle={"italic"}
+              variant={"h4"}
+              align={"center"}
+              gutterBottom
+              data-aos="fade-up"
+              border="1px solid red"
+              width="fit-content"
+              maxWidth="180px"
+            >
+              Habilidades Emocionales
+            </Box>
+            <Box display={"flex"} flexDirection="row">
+              <Box
+                sx={{
+                  display: "flex",
+                  backgroundColor: "blue",
+                  width: "10px",
+                  height: "40px",
+                  transform: "rotate(45deg)",
+                  position: "relative",
+                  right: "10px",
+                }}
+              ></Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  backgroundColor: "blue",
+                  width: "10px",
+                  height: "40px",
+                  transform: "rotate(135deg)",
+                  position: "relative",
+                  left: "10px",
+                }}
+              ></Box>
+            </Box>
+            <Box display={"flex"} flexDirection="row" gap={"10px"}>
+              <Box display={"flex"} flexDirection="column" alignItems="center">
+                <Box
+                  display={"flex"}
+                  fontWeight={700}
+                  color={theme.palette.secondary.main}
+                  fontStyle={"italic"}
+                  variant={"h4"}
+                  align={"center"}
+                  gutterBottom
+                  data-aos="fade-up"
+                  border="1px solid red"
+                  width="fit-content"
+                  maxWidth="180px"
+                >
+                  Consciencia creativa
+                </Box>
+                <Box
+                  display={"flex"}
+                  backgroundColor="blue"
+                  width="10px"
+                  height="40px"
+                ></Box>
+                <Box
+                  display={"flex"}
+                  fontWeight={700}
+                  color={theme.palette.secondary.main}
+                  fontStyle={"italic"}
+                  variant={"h4"}
+                  align={"center"}
+                  gutterBottom
+                  data-aos="fade-up"
+                  border="1px solid red"
+                  width="fit-content"
+                  maxWidth="180px"
+                >
+                  Consciencia de la consciencia
+                </Box>
+                <Box
+                  display={"flex"}
+                  backgroundColor="blue"
+                  width="10px"
+                  height="40px"
+                ></Box>
+                <Box
+                  display={"flex"}
+                  fontWeight={700}
+                  color={theme.palette.secondary.main}
+                  fontStyle={"italic"}
+                  variant={"h4"}
+                  align={"center"}
+                  gutterBottom
+                  data-aos="fade-up"
+                  border="1px solid red"
+                  width="fit-content"
+                  maxWidth="180px"
+                >
+                  Consciencia de la unidad
+                </Box>
+              </Box>
+              <Box display={"flex"} flexDirection="column" alignItems="center">
+                <Box
+                  display={"flex"}
+                  fontWeight={700}
+                  color={theme.palette.secondary.main}
+                  fontStyle={"italic"}
+                  variant={"h4"}
+                  align={"center"}
+                  gutterBottom
+                  data-aos="fade-up"
+                  border="1px solid red"
+                  width="fit-content"
+                  maxWidth="180px"
+                >
+                  Creencias creativas
+                </Box>
+                <Box
+                  display={"flex"}
+                  backgroundColor="blue"
+                  width="10px"
+                  height="40px"
+                ></Box>
+                <Box
+                  display={"flex"}
+                  fontWeight={700}
+                  color={theme.palette.secondary.main}
+                  fontStyle={"italic"}
+                  variant={"h4"}
+                  align={"center"}
+                  gutterBottom
+                  data-aos="fade-up"
+                  border="1px solid red"
+                  width="fit-content"
+                  maxWidth="180px"
+                >
+                  Miendo al bienestar
+                </Box>
+              </Box>
+            </Box>
+          </Box>
         </Container>
       </Box>
     </Box>
-  )
-}
+  );
+};
 
 export default Courses;
